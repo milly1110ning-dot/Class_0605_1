2a1090dba7fd92939d23b5afa2f91f796e748c7a
milly1110ning-dot <milly.1110ning@gmail.com>
2026-06-05 10:03:48 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Person
+{
+	public static void GetName(int a)
+	{
+		Console.WriteLine("上面這段");
+		Console.WriteLine(a);
+	}
+	public static void GetName(float a)
+	{
+		Console.WriteLine("下面這段");
+		Console.WriteLine(float a);
+		return 10;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Class_0605_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person.GetName(10);
+
+        }
+    }
+}
